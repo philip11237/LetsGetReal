aca92f748de2a54e13acb2b069f06b022c281fe7
philip von mueffling <philip.vm3@gmail.com>
2020-11-22 23:18:03 -0500

    1st

---
+++ RealNumber.java
@@ -0,0 +1,67 @@
+public class RealNumber{
+  private double value;
+
+  public RealNumber(double v){
+    value = v;
+  }
+
+  public double getValue(){
+    return value;
+  }
+
+  public String toString(){
+    return ""+getValue();
+  }
+  //---------ONLY EDIT BELOW THIS LINE------------
+
+  /*
+  *Return true when the values are within 0.001% of eachother.
+  *Special case: if one is exactly zero, the other must be exactly zero.
+  */
+  public boolean equals(RealNumber other){
+    if
+     ((this.getValue()==0) && (other.getValue()==0)){
+    return true;
+    }
+    else if ((Math.abs((this.getValue()-other.getValue())/other.getValue())<=.00001)&&((this.getValue()!=0) && (other.getValue()!=0))){
+      return true;
+    }
+    return false;
+  }
+
+  /*
+  *Return a new RealNumber that has the value of:
+  *the sum of this and the other
+  */
+  public RealNumber add(RealNumber other){
+     //other can be ANY RealNumber, including a RationalNumber
+     //or other subclasses of RealNumber (that aren't written yet)
+
+        RealNumber answer=new RealNumber(other.getValue()+ this.getValue());
+     return answer;
+  }
+
+  /*
+  *Return a new RealNumber that has the value of:
+  *the product of this and the other
+  */
+  public RealNumber multiply(RealNumber other){
+        return null;
+  }
+
+  /*
+  *Return a new RealNumber that has the value of:
+  *this divided by the other
+  */
+  public RealNumber divide(RealNumber other){
+        return null;
+  }
+
+  /*
+  *Return a new RealNumber that has the value of:
+  *this minus the other
+  */
+  public RealNumber subtract(RealNumber other){
+    return null;
+  }
+}
